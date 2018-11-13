5414b669ca5bea43364e3068c97e581c40d5bbcd
SimpleStephs <41169466+SimpleStephs@users.noreply.github.com>
2018-11-13 15:51:09 -0600

    A better HW4

--- Homework4.py
+++ Homework4.py
@@ -5,36 +5,41 @@
 # Task: Make 3 feature vectors, that associate with 1, 2, and 3 authors.
 
 import ast
-import numpy.random as nprando
-from sklearn.cluster import KMeans
 
-
-#fv means feature vector
 with open("Test_Normalized_feature_Vectors.txt", "r") as f:
-   fv1 = ast.literal_eval(f.readline())
+   fvs = ast.literal_eval(f.readlines())
+
+  
+# Parameters: takes a feature vector
+# rounds the values to either a 0 or a 1
+# returns a feature vector
+def Round_To_Binary(fv):
     
-#makes a random list filled with -1,0s, and 1s
-fv2 = [nprando.randint(-1,2) for r in xrange(95)]
+    fv = ['%.f' % f for f in fv]
+    
+    return fv
+
+print Round_To_Binary(fvs)
+
+    
+
+# Parameters: takes in two feature vectors
+# This  takes two feature vectors and compares them, if they are similiar 
+# print a 1 if not, -1. If they are the same size 0.
+# returns: a 0, 1, -1
+def Compare_List(fv1, fv2):
+    return 0
+
+# Parameters: List of all the feature vectors, a target fv, the prediction vector
+# Takes in this list of feature vectures and compares it to the target feature vector.
+# Determine how similiar each feature vector is the target by the values 1,0 or -1
+# Returns: a list of size twelve with either a 1, 0 or -1
+def Predict_Author(fvs, targetfv, fv):
+    return fv
+
+
+# makes a random list filled with -1,0s, and 1s
+# fv2 = [nprando.randint(-1,2) for r in xrange(95)]
 
 # multiplies fv1 and fv2 together to get a new list
-fv3 = [a*b for a,b in zip(fv1,fv2)]
-
-# suppose to read in the list of feature vectors and choose 1. I need help
-def authorPredict(fv):
-    aP = KMeans(n_clusters=2, init='k-means++', n_init=10, verbose=0)
-    aP.fit(fv)
-    
-    return aP
-
-print authorPredict(fv3)
-    
-
-
-
-
-
-     
-
-
-
-
+# fv3 = [a*b for a,b in zip(fv1,fv2)]
