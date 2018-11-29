49fd6933c34daba697cf62d2ebf90afb3fadfdaa
Griff Perry <lgp@auburn.edu>
2018-11-29 09:53:27 -0600

    GRNN can now take different k values

--- GRNN.py
+++ GRNN.py
@@ -4,11 +4,17 @@ import operator
 def dist_squared(t_q, t_i):
     sum = 0.0
     for character in range(len(t_q)):
-        sum += math.pow((t_q[character] - t_i[character]), 2)
+        sum += math.pow((t_q[character] - t_i[character]), 2.0)
     return sum
 
 def hf(tq, ti, sigma):
-    return math.exp(-dist_squared(tq, ti)/pow((2.0*sigma), 2.0))
+    answer = 0.0
+    answer = dist_squared(tq, ti)
+    answer = answer * -1
+    answer = answer / pow((2.0*sigma), 2.0)
+    answer = math.exp(answer)
+    return answer
+
 
 def getNeighbors(trainingSet, testInstance, resultCount):
     from homework1 import euclideanDistance
@@ -23,20 +29,32 @@ def getNeighbors(trainingSet, testInstance, resultCount):
         neighbors.append(distances[result][0])
     return neighbors
 
+d = []
+d2 = []
+def list_maker(global_list, listSize, numOfList, element, index):
+
+    count = 0
+    while(count < numOfList):
+        list = [0 for n in range(listSize)]
+        list.insert(index, element)
+        global_list.append(list)
+        count = count + 1
+    return
+
 def compute_fire_strengths(index, test_set, training_set, sigma):
 
-    author1 = list_maker(d, 12, 8, 1, 0)
-    author2 = list_maker(d, 12, 8, 1, 1)
-    author3 = list_maker(d, 12, 8, 1, 2)
-    author4 = list_maker(d, 12, 8, 1, 3)
-    author5 = list_maker(d, 12, 8, 1, 4)
-    author6 = list_maker(d, 12, 8, 1, 5)
+    author1 = list_maker(d, 12, 6, 1, 0)
+    author2 = list_maker(d, 12, 6, 1, 1)
+    author3 = list_maker(d, 12, 6, 1, 2)
+    author4 = list_maker(d, 12, 6, 1, 3)
+    author5 = list_maker(d, 12, 6, 1, 4)
+    author6 = list_maker(d, 12, 6, 1, 5)
     author7 = list_maker(d, 12, 6, 1, 6)
-    author8 = list_maker(d, 12, 8, 1, 7)
-    author9 = list_maker(d, 12, 8, 1, 8)
-    author10 = list_maker(d, 12, 8, 1, 9)
-    author11 = list_maker(d, 12, 8, 1, 10)
-    author12 = list_maker(d, 12, 8, 1, 11)
+    author8 = list_maker(d, 12, 6, 1, 7)
+    author9 = list_maker(d, 12, 6, 1, 8)
+    author10 = list_maker(d, 12, 6, 1, 9)
+    author11 = list_maker(d, 12, 6, 1, 10)
+    author12 = list_maker(d, 12, 6, 1, 11)
 
     i = 0
     dq = []
@@ -48,7 +66,6 @@ def compute_fire_strengths(index, test_set, training_set, sigma):
     practice_set = []
     if (k != 0):
         practice_set = getNeighbors(training_set, test_set, k)
-
     else:
         practice_set = training_set
 
@@ -56,8 +73,17 @@ def compute_fire_strengths(index, test_set, training_set, sigma):
     for trainthing in practice_set:
         hfs.append(hf(test_set, trainthing, sigma))
 
-    temp_d = list(d)
-    temp_d.pop(index)
+    if (k == 0):
+        temp_d = list(d)
+        temp_d.pop(index)
+
+    if (k != 0):
+        temp_d = []
+        for author in practice_set:
+            new_index = author[-1]
+            list_maker(temp_d, 12, 1, 1, new_index)
+
+
     for i in range(len(practice_set)):
         for j in range(len(dq)):
             dq[j] += hfs[i]*temp_d[i][j]
@@ -71,17 +97,6 @@ def compute_fire_strengths(index, test_set, training_set, sigma):
             dq[j] = dq[j]/sum_hfs
     return dq
 
-d = []
-d2 = []
-def list_maker(global_list, listSize, numOfList, element, index):
-
-    count = 0
-    while(count < numOfList):
-        list = [0 for n in range(listSize)]
-        list.insert(index, element)
-        global_list.append(list)
-        count = count + 1
-    return
 
 def compute_fire_strengths_CASIS25(index, test_set, training_set, sigma):
 
@@ -129,8 +144,16 @@ def compute_fire_strengths_CASIS25(index, test_set, training_set, sigma):
     for trainthing in practice_set:
         hfs.append(hf(test_set, trainthing, sigma))
 
-    temp_d = list(d2)
-    temp_d.pop(index)
+    if (k == 0):
+        temp_d = list(d2)
+        temp_d.pop(index)
+
+    if (k != 0):
+        temp_d = []
+        for author in practice_set:
+            new_index = author[-1]
+            list_maker(temp_d, 25, 1, 1, new_index)
+
     for i in range(len(practice_set)):
         for j in range(len(dq)):
             dq[j] += hfs[i]*temp_d[i][j]
